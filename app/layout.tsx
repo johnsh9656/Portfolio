75111c0a5e131c54fc72fce8ebe599a998934dd4
johnsh9656 <harrisonjohns1219@gmail.com>
2024-10-06 18:03:28 -0400

    Update website Head, README.md

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <Head>
+        {/* Primary Meta Tags */}
+        <title>Harrison Johns</title>
+        <meta name="title" content="Harrison Johns" />
+        <meta name="description" content="Software Engineering Student Portfolio" />
+
+        {/* Open Graph / Facebook Meta Tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://harrisonjohns.me/" />
+        <meta property="og:title" content="Harrison Johns" />
+        <meta property="og:description" content="Software Engineering Student Portfolio" />
+        <meta property="og:image" content="https://harrisonjohns.me/favicon.ico" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="https://harrisonjohns.me/images/porthead.png" />
+        <meta name="twitter:url" content="https://harrisonjohns.me/" />
+        <meta name="twitter:title" content="Harrison Johns" />
+        <meta name="twitter:description" content="Software Engineering Student Portfolio" />
+        <meta name="twitter:image" content="https://harrisonjohns.me/favicon.ico" />
+      </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
