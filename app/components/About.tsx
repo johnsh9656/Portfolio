dfec8959d3538ec18bf771273da9eefaf7fea6df
johnsh9656 <harrisonjohns1219@gmail.com>
2025-05-07 13:12:17 -0400

    fix hydration error in about section (replace nested <p> tags with <h2> tags)

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -15,10 +15,11 @@ const About = () => {
   }
 
   const about = <>
-    <p className='mb-2'><span className='border-b-2 border-accent'>Hello!</span>  I'm Harrison — a software developer and Software Engineering student at McMaster University. I like building things that are practical, clean, and actually useful. My background is mainly in full-stack development, but I've also worked on desktop apps, game projects, and machine learning integrations.</p>
+    <p className='mb-2'><span className='border-b-2 border-accent'>Hello!</span>  I'm Harrison — a software developer and Software Engineering student at McMaster University. I like building things that are practical, clean, and actually useful. My background is mainly in full-stack development, but I've also worked on video game development and machine learning projects.</p>
     <p className='mb-2'>I'm someone who learns by doing — whether that's picking up a new tech stack for a project, troubleshooting hardware and software, or refining the way a system fits together. I care about writing code that's not just functional but understandable, maintainable, and well-designed.</p>
     <p/>
-    <p>Outside of coding, I'm always working on something — whether that's fencing competitively, learning a new instrument, or chasing new ideas in music, tech, or design. I like working with teams where people take what they do seriously without taking themselves too seriously — where good work and good conversations happen side by side.</p>
+    <p className='mb-2'>Outside of coding or designing, I'm always working on something — whether that's fencing, learning a new instrument, challenging my existing skills, or exploring new ideas in music, tech, etc.. I love working with people who are passionate about their work and set high standards while being willing to challenge their assumptions and skills.</p>
+    <p className=''>I’m currently looking for a co-op position starting in Summer/Fall 2025. If you think I’d be a good fit for your team, please reach out!</p>
   </>
 
 
@@ -36,9 +37,9 @@ const About = () => {
           <h1 className='heading text-left mb-4'>
             About Me 📝<span className='text-purple-200'></span>
           </h1>
-          <p className='text-lg text-gray-300'>
+          <h2 className='text-lg text-gray-300'>
             {about}
-          </p>
+          </h2>
           <button onClick={() => window.open('/resume.pdf')} className='mt-4 w-[5.6rem] py-1 bg-accent text-white rounded-2xl align-middle text-center text-md hover:bg-secondary'>
             Resume
           </button>
